983416fc58b27f8caf2377d2dbd0fc53610a9498
Bituq <d.noorland9@hotmail.com>
2023-06-22 18:24:14 +0000

    Add comments and local temp file

--- simulator.sh
+++ simulator.sh
@@ -1,74 +1,79 @@
 #!/bin/bash
 
-# Check if the correct number of arguments is provided
 if [ "$#" -ne 2 ] || [ "$1" != "-file" ]; then
-    echo "Usage: $0 -file <input_file>"
+    echo "Usage: $0 -file <file>"
     exit 1
 fi
 
-input_file="$2"
+file="$2"
 
-# Check if the input file exists and is readable
-if [ ! -r "$input_file" ]; then
-    echo "Error: Cannot read input file '$input_file'"
+if [ ! -r "$file" ]; then
+    echo "Error: Cannot read input file '$file'"
     exit 1
 fi
 
-# Create a temporary file to store the ready queue
-ready_queue=$(mktemp)
+# Create a temp file for the ready queue
+# This is done using mktemp which generates a unique temp file name and creates an empty file with that name.
+# This is useful to make ensure that there are no conflicts in file names.
+queue_temp=$(mktemp ./ready_queue.XXXXXX)
 if [ $? -ne 0 ]; then
     echo "Error: Failed to create temporary file for ready queue"
     exit 1
 fi
 
-trap 'rm -f "$ready_queue"' EXIT
+# This command ensures that the temp file is deleted when the scripts exits, even during an error.
+# This makes sure that there are no temp files cluttered in the file system.
+trap 'rm -f "$queue_temp"' EXIT
 
-# Load the processes into the ready queue and validate the input file's content
-process_id=0
-while IFS=',' read -r process_name arrival_time burst_time || [[ -n "$process_name" ]]; do
-    # Validate the input file's content (process name, arrival time, and burst time)
-    if [[ ! "$process_name" =~ ^[A-Za-z0-9]+$ ]] || [[ ! "$arrival_time" =~ ^[0-9]+$ ]] || [[ ! "$burst_time" =~ ^[0-9]+$ ]]; then
+# Load processes into the ready queue and validate input file content
+proc_id=0
+while IFS=',' read -r proc_name arrival burst || [[ -n "$proc_name" ]]; do
+    # Validate input file content (process name, arrival time, and burst time)
+    if [[ ! "$proc_name" =~ ^[A-Za-z0-9]+$ ]] || [[ ! "$arrival" =~ ^[0-9]+$ ]] || [[ ! "$burst" =~ ^[0-9]+$ ]]; then
         echo "Error: Invalid data in input file"
         exit 1
     fi
-    # Define a unique process name to ensure duplicate processes are handled properly
-    unique_process_name="${process_name}_${process_id}"
-    echo "$unique_process_name,$arrival_time,$burst_time,$process_id" >> "$ready_queue" 
-    process_id=$((process_id + 1))
-done < "$input_file"
+    # Define a unique process name to handle duplicate processes properly
+    unique_proc_name="${proc_name}_${proc_id}"
+    echo "$unique_proc_name,$arrival,$burst,$proc_id" >> "$queue_temp" 
+    proc_id=$((proc_id + 1))
+done < "$file"
 
 # Sort the ready queue based on arrival time and process ID
-# This ensures that processes are executed in the order they arrive and maintain fairness
-sort -t',' -k2,2n -k4,4n -o "$ready_queue" "$ready_queue"
+sort -t',' -k2,2n -k4,4n -o "$queue_temp" "$queue_temp"
 
 current_time=0
 
 # Run the scheduler until the ready queue is empty
-while [ "$(wc -l < "$ready_queue")" -gt 0 ]; do
-    # Check if any process has arrived
-    arrived_process=$(awk -F',' -v time="$current_time" '$2 <= time { print; exit }' "$ready_queue")
+# The wc -l command counts the lines, and the loop continues as long as there is at least one process in the queue.
+while [ "$(wc -l < "$queue_temp")" -gt 0 ]; do
+    # The awk command is used to check if any process has arrived by comparing the arrival time with the current time.
+    arrived=$(awk -F',' -v time="$current_time" '$2 <= time { print; exit }' "$queue_temp")
 
-    if [ -n "$arrived_process" ]; then
-        unique_process_name=$(echo "$arrived_process" | cut -d',' -f1)
-        arrival_time=$(echo "$arrived_process" | cut -d',' -f2)
-        burst_time=$(echo "$arrived_process" | cut -d',' -f3)
-        process_name=$(echo "$unique_process_name" | cut -d'_' -f1)
+    if [ -n "$arrived" ]; then
+        # The cut command is used to extract the unique proc name, arrival, and burst time. The original process name is also extracted by removing the appended process ID.
+        unique_proc_name=$(echo "$arrived" | cut -d',' -f1)
+        arrival=$(echo "$arrived" | cut -d',' -f2)
+        burst=$(echo "$arrived" | cut -d',' -f3)
+        proc_name=$(echo "$unique_proc_name" | cut -d'_' -f1)
 
         # Execute the process for one quantum
-        echo -e "$process_name is using the CPU"
-        burst_time=$((burst_time - 1))
+        echo -e "$proc_name is using the CPU"
+        burst=$((burst - 1))
 
-        # Update the ready queue
-        grep -v "^$unique_process_name," "$ready_queue" > "${ready_queue}.tmp"
-        mv "${ready_queue}.tmp" "$ready_queue"
+        # grep -v is used to remove the executed process from the ready queue by excluding lines that start with the unique process name.
+        # The updated queue is written to a temp file, which is then renamed to replace the original queue file.
+        grep -v "^$unique_proc_name," "$queue_temp" > "${queue_temp}.tmp"
+        mv "${queue_temp}.tmp" "$queue_temp"
 
         # If the process has not finished, add it back to the ready queue
-        if [ "$burst_time" -gt 0 ]; then
-            process_id=$((process_id + 1))
-            echo "$unique_process_name,$((current_time + 1)),$burst_time,$process_id" >> "$ready_queue"
-            sort -t',' -k2,2n -k4,4n -o "$ready_queue" "$ready_queue"
+        # The ready queue is then sorted again based on arrival and PID to maintain the correct order of processes.
+        if [ "$burst" -gt 0 ]; then
+            proc_id=$((proc_id + 1))
+            echo "$unique_proc_name,$((current_time + 1)),$burst,$proc_id" >> "$queue_temp"
+            sort -t',' -k2,2n -k4,4n -o "$queue_temp" "$queue_temp"
         else
-            echo -e "Process $process_name terminated"
+            echo -e "Process $proc_name terminated"
         fi
     else
         echo "idle"
